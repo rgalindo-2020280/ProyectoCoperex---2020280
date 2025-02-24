cc9b2e90e513a90a5129aa8b489510d69f50458c
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-24 11:28:50 -0600

    Se agrega la configuracion al archivo validate.jwt.js

---
+++ middlewares/validate.jwt.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import jwt from 'jsonwebtoken'
+import { existUserById } from '../helpers/db.validators'
+
+export const validateJwt = async (req, res, next) => {
+    try {
+        let secretKey = process.env.SECRET_KEY
+        let { authorization } = req.headers
+        if (!authorization) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
+        let user = jwt.verify(authorization, secretKey)
+        const userExists = await existUserById(user.uid)
+        if (!userExists) {
+            return res.status(404).send({ message: 'User not found or removed' })
+        }
+        req.user = user
+        next()
+    } catch (err) {
+        console.error('JWT Error:', err);
+        return res.status(401).send({ message: 'Invalid token or expired' })
+    }
+}
+
+export const isAdmin = (req, res, next) => {
+    if (req.user.role !== 'ADMIN') {
+        return res.status(403).send({ message: 'Access denied, insufficient privileges' })
+    }
+    next()
+}
