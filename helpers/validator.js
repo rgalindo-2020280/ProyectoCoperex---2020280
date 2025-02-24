fbdf5c81c532ec5e8ee51610ed47167a8431cdd4
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-24 16:44:04 -0600

    Se agregan validaciones para el registro y login de usuario

---
+++ helpers/validator.js
@@ -0,0 +1,39 @@
+import { body } from 'express-validator'
+import { validateErrorWithoutImg } from './validate.error.js'
+import { existUsername, existEmail, objectIdValid } from './db.validators.js'
+
+export const registerValidator = [
+    body('name', 'Name cannot be empty')
+        .notEmpty(),
+    body('surname', 'Surname cannot be empty')
+        .notEmpty(),
+    body('email', 'Email cannot be empty or is not a valid email')
+        .notEmpty()
+        .isEmail()
+        .custom(existEmail),
+    body('username', 'Username cannot be empty')
+        .notEmpty()
+        .toLowerCase()
+        .custom(existUsername),
+    body('password', 'Password cannot be empty')
+        .notEmpty()
+        .isStrongPassword()
+        .withMessage('The password must be strong')
+        .isLength({min: 8}),
+    body('phone', 'Phone cannot be empty or is not a valid phone')
+        .notEmpty()
+        .isMobilePhone(),
+    validateErrorWithoutImg
+]
+
+export const loginValidator = [
+    body('userLoggin', 'Username or email cannot be empty')
+        .notEmpty()
+        .toLowerCase(),
+    body('password', 'Password cannot be empty')
+        .notEmpty()
+        .isStrongPassword()
+        .withMessage('The password must be strong')
+        .isLength({min: 8}),
+    validateErrorWithoutImg
+]
