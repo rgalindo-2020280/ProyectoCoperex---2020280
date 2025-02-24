12171c5f945c3ec3aa55173586f4638f9deb7ac5
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-24 16:26:35 -0600

    Se agrega el modelo de compañía

---
+++ src/company/company.model.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose'
+
+const companySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Company name is required"],
+            unique: true,
+            maxLength: [100, "Cannot exceed 100 characters"]
+        },
+        companySize: {
+            type: String,
+            required: [true, "Impact level is required"],
+            enum: ["Low", "Medium", "High"],
+            default: "Medium"
+        },
+        yearsInService: {
+            type: Number,
+            required: [true, "Years in business is required"],
+            min: [0, "Years cannot be negative"]
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: "Category",
+            required: [true, "Category is required"]
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+export default model("Company", companySchema)
+
+
