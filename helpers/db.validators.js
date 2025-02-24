4148ff09b027a3eb38aa44065cae4b1f6db8f9e8
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-24 11:23:52 -0600

    Se agrega la configuracion al archivo db.validators.js

---
+++ helpers/db.validators.js
@@ -0,0 +1,32 @@
+import User from '../src/user/user.model'
+import { isValidObjectId } from 'mongoose'
+
+export const existUsername = async(username)=>{
+    const alreadyUsername = await User.findOne({username})
+    if(alreadyUsername){
+        console.error(`Username ${username} is already taken`)
+        throw new Error(`Username ${username} is already taken`)
+    }
+}
+
+export const existEmail = async(email)=>{
+    const alreadyEmail = await User.findOne({email}) 
+        if(alreadyEmail){
+        console.error(`Email ${email} is already taken`)
+        throw new Error(`Email ${email} is already taken`)
+    }
+}
+
+export const objectIdValid = async(objectId)=>{    
+    if(!isValidObjectId(objectId)){
+        throw new Error(`Keeper is not objectId valid`)
+    }
+}
+
+export const existUserById = async (uid) => {
+    const user = await User.findById(uid)
+    if (!user) {
+        return false
+    }
+    return true
+}
