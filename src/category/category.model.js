6a58fb2bdac527fcdb701c796c0efe3fb674289c
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-24 16:18:06 -0600

    Se agrega modelo de categoria

---
+++ src/category/category.model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose'
+
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Category name is required"],
+            unique: true,
+            maxLength: [50, "Cannot exceed 50 characters"]
+        },
+        description: {
+            type: String,
+            maxLength: [250, "Cannot exceed 250 characters"]
+        }
+    }
+)
+
+const Category = model("Category", categorySchema)
+
+const createCategory = async () => {
+    try {
+        const categoryExists = await Category.findOne({ name: "General" })
+        if (!categoryExists) {
+            const defaultCategory = new Category({
+                name: "General", 
+                description: "Default category"
+            })
+            await defaultCategory.save();
+            console.log("Default category created");
+        }
+    } catch (error) {
+        console.error("Error creating default category:", error)
+    }
+}
+
+createCategory()
+export default Category
