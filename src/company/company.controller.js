2709424549220dc4518d7fd79c8d6bb178e88be2
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-25 17:21:13 -0600

    Se agrega la funcion update al company.controller.js

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -53,3 +53,57 @@ export const addCompany = async (req, res) => {
         })
     }
 }
+
+export const updateCompany = async (req, res) => {
+    try {
+        const { name, impactLevel, yearsInBusiness, category } = req.body
+        const { id } = req.params
+        const company = await Company.findById(id)
+        if (!company) {
+            return res.status(404).send({
+                success: false,
+                message: 'Company not found'
+            })
+        }
+        if (category) {
+            const existingCategory = await Category.findById(category)
+
+            if (!existingCategory) {
+                return res.status(404).send({
+                    success: false,
+                    message: 'Category not found'
+                })
+            }
+        }
+        const updatedCompany = await Company.findByIdAndUpdate(
+            id,
+            { name, impactLevel, yearsInBusiness, category },
+            { new: true }
+        )
+        if (category && category.toString() !== company.category.toString()) {
+            await Category.findByIdAndUpdate(company.category, {
+                $pull: { companies: company._id }
+            })
+
+            await Category.findByIdAndUpdate(category, {
+                $push: { companies: updatedCompany._id }
+            })
+        }
+        const populatedCompany = await Company.findById(updatedCompany._id)
+            .populate({
+                path: 'category',
+                select: 'name description -_id'
+            })
+        return res.send({
+            success: true,
+            message: 'Company updated successfully',
+            updatedCompany: populatedCompany
+        })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({
+            success: false,
+            message: 'General error'
+        })
+    }
+}
