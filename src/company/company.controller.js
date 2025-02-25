c39c34fdd894482a8c402b9d220db9046746ef7c
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-02-24 19:54:49 -0600

    Se cambia la funcion de añadir de compañía controller para una elaborada de mejor manera

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -4,13 +4,25 @@ import Category from "../category/category.model.js"
 export const addCompany = async (req, res) => {
     try {
         const { name, impactLevel, yearsInBusiness, category } = req.body
+        if (!name || !impactLevel || yearsInBusiness === undefined || !category) {
+            return res.status(400).send({
+                success: false,
+                message: "All fields required"
+            })
+        }
         const existingCategory = await Category.findById(category)
         if (!existingCategory) {
-            return res.status(404).send("Category not found")
+            return res.status(404).send({
+                success: false,
+                message: "Category not found"
+            })
         }
         const existingCompany = await Company.findOne({ name })
         if (existingCompany) {
-            return res.status(400).send("Company already exists")
+            return res.status(400).send({
+                success: false,
+                message: "Company already exists"
+            })
         }
         const newCompany = new Company({
             name,
@@ -21,9 +33,23 @@ export const addCompany = async (req, res) => {
         const savedCompany = await newCompany.save()
         existingCategory.companies.push(savedCompany._id)
         await existingCategory.save()
+        const populatedCompany = await Company.findById(savedCompany._id)
+            .populate({
+                path: 'category',
+                select: 'name description -_id'
+            })
 
-        res.status(201).send("Company added successfully")
+        return res.status(201).send({
+            success: true,
+            message: "Company added successfully",
+            company: populatedCompany
+        })
+
     } catch (error) {
-        res.status(500).send("Error adding company")
+        return res.status(500).send({
+            success: false,
+            message: "Error adding company",
+            error: error.message
+        })
     }
 }
