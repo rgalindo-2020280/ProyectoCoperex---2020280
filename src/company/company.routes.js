1ba31793f06abe768690091be25edb77b83f70b0
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-03-02 12:54:28 -0600

    Se agregan las validaciones a las rutas

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -8,18 +8,21 @@ import {
 } from './company.controller.js'
 
 import { validateJwt } from '../../middlewares/validate.jwt.js'
+import { addCompanyValidator, updateCompanyValidator} from '../../helpers/validator.js'
 
 const api = Router()
 
 api.post(
     '/addCompany',
     [validateJwt],
+    [addCompanyValidator],
     addCompany
 )
 
 api.put(
     '/:id',
     [validateJwt],
+    [updateCompanyValidator],
     updateCompany
 )
 
