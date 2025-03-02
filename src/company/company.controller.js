dc010232ed3625930c96163651ca3b1503471f56
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-03-02 12:53:29 -0600

    Se modifican las funciones de agregar y actualizar

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -4,35 +4,21 @@ import Category from "../category/category.model.js"
 export const addCompany = async (req, res) => {
     try {
         const { name, impactLevel, yearsInBusiness, category } = req.body
-        if (!name || !impactLevel || yearsInBusiness === undefined || !category) {
-            return res.status(400).send({
-                success: false,
-                message: "All fields required"
-            })
-        }
+
         const existingCategory = await Category.findById(category)
-        if (!existingCategory) {
-            return res.status(404).send({
-                success: false,
-                message: "Category not found"
-            })
-        }
-        const existingCompany = await Company.findOne({ name })
-        if (existingCompany) {
-            return res.status(400).send({
-                success: false,
-                message: "Company already exists"
-            })
-        }
+
         const newCompany = new Company({
             name,
             impactLevel,
             yearsInBusiness,
             category
         })
+
         const savedCompany = await newCompany.save()
+
         existingCategory.companies.push(savedCompany._id)
         await existingCategory.save()
+
         const populatedCompany = await Company.findById(savedCompany._id)
             .populate({
                 path: 'category',
@@ -65,9 +51,9 @@ export const updateCompany = async (req, res) => {
                 message: 'Company not found'
             })
         }
+
         if (category) {
             const existingCategory = await Category.findById(category)
-
             if (!existingCategory) {
                 return res.status(404).send({
                     success: false,
@@ -75,11 +61,13 @@ export const updateCompany = async (req, res) => {
                 })
             }
         }
+
         const updatedCompany = await Company.findByIdAndUpdate(
             id,
             { name, impactLevel, yearsInBusiness, category },
             { new: true }
         )
+
         if (category && category.toString() !== company.category.toString()) {
             await Category.findByIdAndUpdate(company.category, {
                 $pull: { companies: company._id }
@@ -89,16 +77,19 @@ export const updateCompany = async (req, res) => {
                 $push: { companies: updatedCompany._id }
             })
         }
+
         const populatedCompany = await Company.findById(updatedCompany._id)
             .populate({
                 path: 'category',
                 select: 'name description -_id'
             })
+
         return res.send({
             success: true,
             message: 'Company updated successfully',
             updatedCompany: populatedCompany
         })
+
     } catch (err) {
         console.error(err)
         return res.status(500).send({
