8de8a54df20e5f0d74b072607c883557e2ca298c
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-03-02 10:49:21 -0600

    Se agrega validacion para agregar categoria

--- helpers/validator.js
+++ helpers/validator.js
@@ -37,3 +37,34 @@ export const loginValidator = [
         .isLength({min: 8}),
     validateErrorWithoutImg
 ]
+
+export const categoryValidator = [
+    body('name')
+        .notEmpty().withMessage('Category name cannot be empty')
+        .isString().withMessage('Category name must be a valid string')
+        .isLength({ max: 50 }).withMessage('Category name must not exceed 50 characters')
+        .custom(async (name) => {
+            const existingCategory = await Category.findOne({ name })
+            if (existingCategory) {
+                throw new Error('Category already exists')
+            }
+        }),
+    body('description')
+        .optional()
+        .isString().withMessage('Description must be a string')
+        .isLength({ max: 250 }).withMessage('Description must not exceed 250 characters'),
+    body('companyIds')
+        .optional()
+        .isArray().withMessage('Company IDs must be an array')
+        .custom(async (companyIds) => {
+            if (!Array.isArray(companyIds)) {
+                throw new Error('Company IDs must be an array')
+            }
+
+            const existingCompanies = await Company.find({ _id: { $in: companyIds } })
+            if (existingCompanies.length !== companyIds.length) {
+                throw new Error('Some companies do not exist')
+            }
+        }
+    )
+]
